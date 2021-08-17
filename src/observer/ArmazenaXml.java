631a0e04d53dbee00d14d6c0500affc4d5bd140f
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2021-08-16 21:44:09 -0300

    add example observer pattern

--- src/observer/ArmazenaXml.java
+++ src/observer/ArmazenaXml.java
@@ -1,6 +1,7 @@
 package observer;
 
 public class ArmazenaXml implements NotaFiscalEmitidaListener {
+
     @Override
     public void update(NotaFiscal notaFiscal) {
         System.out.printf("\n XML da nota fiscal %s armazenado!", notaFiscal.getNumero());
